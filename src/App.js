98d5701d5333349dca7e491be383fd6c1e649edf
cod-lvr <aymang29@yahoo.com>
2021-08-30 11:35:47 +0200

    add setting up files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route ></Route>
+        <Route ></Route>
+        <Route ></Route>
+        <Route ></Route>
+      </Switch>  
+    </div>
+  );
+}
+
+export default App;
