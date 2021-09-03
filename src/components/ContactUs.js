d7343282b5760394bcae74f2a9a44effb0eb3992
cod-lvr <aymang29@yahoo.com>
2021-09-04 00:55:26 +0200

    adding contactUs component

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,7 +1,27 @@
+
+import Form from "./Form"
+import classes from './ContactUs.module.css';
+
 const ContactUs = () => {
-    return (
-        <h2>heloo</h2>
-    ) 
-}
+  return (
+    <main className={classes["main"]}>
+      <h3>contact us</h3>
+      <div className={classes["box"]}>
+        <div className={classes["content-box"]}>
+          <p>
+            making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident,
+          </p>
+        </div>
+        <div className={classes["form"]}>
+            <Form />
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default ContactUs;
